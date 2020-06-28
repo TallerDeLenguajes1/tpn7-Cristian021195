0fd6d066a2a2410a83b3773d8b5ea0fa345d0a97
Cristian021195 <cristiangramajo015@gmail.com>
2020-06-27 22:26:36 -0300

    Se agrego carpeta con la clase calculadora,

---
+++ Clases/Calculadora.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace tpn7.Clases
+{
+    public class Calculadora
+    {
+        private float x;
+        private float y;
+        private string z;
+
+        public float X { get => x; set => x = value; }
+        public float Y { get => y; set => y = value; }
+        public string Z { get => z; set => z = value; }
+
+        public string resultado(float X, float Y, string Z) {
+            string res="";
+            if (Z == "+") {
+                res = (X + Y).ToString();
+            }else if(Z == "-")
+            {
+                res = (X - Y).ToString();
+            }
+            else if(Z == "*")
+            {
+                res = (X * Y).ToString();
+            }
+            else if(Z == "/")
+            {
+                if (Y != 0) {
+                    res = (X / Y).ToString();
+                }
+                else { res = "Infinito"; }
+            }
+
+            return res;
+        }
+    }
+}
